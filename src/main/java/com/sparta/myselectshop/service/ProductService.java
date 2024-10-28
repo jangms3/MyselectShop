d03f8bcf6ad927eb69416a334b886ee4ada1a17e
jangms3 <github.com/jangms3>
2024-10-29 08:53:19 +0900

    관심ㅁì상품 희망 최저가 업데이트 api

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -1,11 +1,16 @@
 package com.sparta.myselectshop.service;
 
 import com.sparta.myselectshop.Repository.ProductRepository;
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +18,26 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    public static final int MIN_MY_PRICE = 100;
+
     public ProductResponseDto createProduct(ProductRequestDto requestDto) {
         Product product = productRepository.save(new Product(requestDto));
+        return new ProductResponseDto(product);
+    }
+
+    @Transactional
+    @PutMapping("/products/{id}")
+    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+        int myprice = requestDto.getMyprice(); //100원 이상이여야 함.
+        if (myprice < MIN_MY_PRICE){
+            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다.최소" + MIN_MY_PRICE+ "원 이상으로 설정해주세요");
+        }
+        Product product = productRepository.findById(id).orElseThrow(()->
+                new NullPointerException("해당 상품을 찾을 수 없습니다")
+        );
+
+        product.update(requestDto);
+
         return new ProductResponseDto(product);
     }
 }
