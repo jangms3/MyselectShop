af4e3052244522e7bb9b4d06d74cfa8053a4be6e
jangms3 <github.com/jangms3>
2024-10-29 08:43:01 +0900

    ê관심상품 등록  api

---
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.sparta.myselectshop.controller;
+
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping("/products")
+    public ProductResponseDto createProduct(@RequestParam ProductRequestDto productRequestDto) {
+        return null;
+    }
+
+}
